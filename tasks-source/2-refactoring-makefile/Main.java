5512344f2a33e8ad09f8cb3e9bfda04a312d550d
jancajthaml <jan.cajthaml@gmail.com>
2013-09-27 14:08:34 +0200

    Read repaired …. someone on the server killed with CTRL+C soooo null pointer exception was thrown

--- tasks-source/2-refactoring-makefile/Main.java
+++ tasks-source/2-refactoring-makefile/Main.java
@@ -5,45 +5,48 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.NoSuchElementException;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main
 {
-    static int BIG_ENOUGHT_LINES               = 100000;
-    static int BIG_ENOUGHT_NEIGHBOURS          = 1000;
-    static BufferedReader bi                   = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuilder buffer                = new StringBuilder();
-    public static HashMap<String,Node> cache   = new HashMap<String,Node>();
-    static String[] line_map                   = new String[BIG_ENOUGHT_LINES];
-    static String[] relation                   = new String[BIG_ENOUGHT_LINES];
-    static int LINES_RED                       = 0;
-    static String s                            = "";
-    static String name                         = "";
-    static int current                         = 0;	
-    static Queue<Node> q                       = new LinkedList<Node>();
+	static  String newline = System.getProperty("line.separator");
+
+    static  int                   BIG_ENOUGHT_LINES       =  10000;
+    static  int                   BIG_ENOUGHT_NEIGHBOURS  =  100;
+    static  BufferedReader        bi                      =  new BufferedReader(new InputStreamReader(System.in));
+    static  StringBuilder         buffer                  =  new StringBuilder();
+    static  HashMap<String,Node>  cache                   =  new HashMap<String,Node>();
+    static  String[]              line_map                =  new String[BIG_ENOUGHT_LINES];
+    static  String[]              relation                =  new String[BIG_ENOUGHT_LINES];
+    static  int                   LINES_RED               =  0;
+    static  String                name                    =  "";
+    static  int                   current                 =  0;	
+    static  Queue<Node>           q                       =  new LinkedList<Node>();
+    static String FIRST = "";
 
     public static void main(String[] args) throws IOException
     {
-    	bi = new BufferedReader(new InputStreamReader(System.in));
     	
         buffer.setLength(0);
         cache.clear();
         q.clear();
-        
         LINES_RED = 0;
-        s         = "";
+        FIRST     = "";
         name      = "";
         current   = 0;
-
+        bi        = new BufferedReader(new InputStreamReader(System.in));
+    	
         try
         {
             slurp();
 
-            Node root = cache.get("all");
+            Node root = cache.get(FIRST);
 
-            unlinked();
+//            unlinked();
             traverse(root);
-            circle(root);
+          //  circle(root);
 
             out();
         }
@@ -68,106 +71,104 @@ public class Main
         }
         return n;
     }
-
-    private static void readLine() throws FatalError
+    
+    static StringTokenizer tokenizer;
+    
+    private static void readLine(String line) throws FatalError
 	{
-		line_map[LINES_RED++] = s;
-		
-		if(s.charAt(0) != 9)
-		{
-			try
+    	if(line.trim().isEmpty()) return;
+    	
+    	line_map[LINES_RED++] = line;
+    	
+    	if (line.startsWith("\t"))
+    	{
+            relation[LINES_RED-1] = name;
+		}
+    	else
+    	{
+	        tokenizer             = new StringTokenizer(line);
+			
+            try
+            { name = tokenizer.nextToken().replace(":", ""); }
+            catch(NoSuchElementException e)
+            { return; }
+            
+            if(FIRST.equalsIgnoreCase("")) FIRST=name;
+            
+            Node n				    = null;
+
+            if(!name.equals(FIRST) && !cache.containsKey(name))
+            {
+                n      = node(name);
+                n.dead = true;
+            }
+            else n = node(name);
+			
+            n.isDeclared=true;
+
+			while(tokenizer.hasMoreTokens())
 			{
-				int index				= s.indexOf(":");
-				name					= s.substring(0, index);
-				String[] dependencies	= s.substring(index).split(" ");
-				Node n				    = null;
-				
-				if(!name.equals("all") && !cache.containsKey(name))
-				{
-					n = node(name);
-					n.dead=true;
-				}
-				else n = node(name);
-				
-				n.isDeclared=true;
-
-				for(int i=1; i<dependencies.length; i++)
-				{
-					if(dependencies[i].isEmpty()) continue;
-					Node m = node(dependencies[i]);
-					n.addEdge(m);
-					m.isDependency=true;
-					//edges.add(new Edge(n,m));
-				}
-				relation[LINES_RED-1]= name;
+                Node m = node(tokenizer.nextToken());
+                n.addEdge(m);
+                m.isDependency = true;
+                
 			}
-			catch(StringIndexOutOfBoundsException e){}
+            relation[LINES_RED-1]= name;
 		}
-		else
-		{
-			relation[LINES_RED-1]=name;
-		}
-		
-		s="";
-	}
-	
-	
-	public static void slurp() throws FatalError,IOException
-	{
-		while(((s=bi.readLine()) != null) && s.length()>0)
-			readLine();
-	}
+    }
 
-	static void out() throws FatalError
-	{
-		buffer.setLength(0);
-		
-		Node n = null;
-		for(int i=0; i<LINES_RED; i++)
-		{
+    public static void slurp() throws FatalError,IOException
+    {
+    	String line="";
+    	while ((line = bi.readLine()) != null)
+    	{ readLine(line); }
+    }
+       
+
+    static void out() throws FatalError
+    {
+        buffer.setLength(0);
+
+        Node n = null;
+
+        for(int i=0; i<LINES_RED; i++)
+        {
 			n = cache.get(relation[i]);
-			
-			if(n.dead && n.visited)             throw new FatalError();
+			if(n==null) continue;
+			if(n.dead && n.visited) throw new FatalError();
 
+			if(n.visited) 	buffer.append(line_map[i]+newline);
+			else			buffer.append("#"+line_map[i]+newline);
+        }
+		
+        System.out.print(buffer);
+    }
+
+    public static void circle(Node node) throws FatalError
+    {
+        for(int i=0; i<node.index; i++)
+        {
+            Node n   = node.link[i];
+            n.marked = true;
 			
-			if(n.visited) 	buffer.append(line_map[i]+"\n");
-			else			buffer.append("#"+line_map[i]+"\n");
-		}
-		
-		System.out.print(buffer);
-	}
-	
-	private static void dumpLines()
-	{
-		for(int i=0; i<LINES_RED; i++)
-			System.out.println(line_map[i]);
-		
-	}
-	
-	public static void circle(Node node) throws FatalError
-	{
-		for(int i=0; i<node.index; i++)
-		{
-			Node n    = node.link[i];
-			n.marked = true;
-			
-			if(circle(n,node))
-			{
-				n.marked = false;
-				throw new FatalError();
-			}
-			n.marked = false;
-		}
-	}
-	
-	public static boolean circle(Node node, Node origin)
-	{
-		
+            if(circle(n,node))
+            {
+                n.marked = false;
+                throw new FatalError();
+            }
+            n.marked = false;
+        }
+    }
+
+    public static boolean circle(Node node, Node origin)
+    {		
 		for(int i=0; i<node.index; i++)
 		{
 			Node n = node.link[i];
-			if(n==null) continue;
+
+			if(n==null)                   continue;
 			if(origin.marked && n.marked) return true;
+
 			if(node.marked && node.visited)
 			{
 				n.marked=true;
@@ -201,18 +202,16 @@ public class Main
 					adj.visited = true;
 					q.add(adj);
 				}
-				//else throw new FatalError(adj.toString());
+
 			}
 		}
 	}
 
-	static class FatalError extends RuntimeException
-	{
-		
-	}
+    static class FatalError extends RuntimeException
+    { private static final long serialVersionUID = 3638938829930139263L; }
 
-	static class Node
-	{
+    static class Node
+    {
 		String data       = "";
 		boolean visited   = false;
 		boolean marked	  = false;
