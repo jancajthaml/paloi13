560ff10f972764bfd53c3dadeba326f8f267d15f
jancajthaml <jan.cajthaml@gmail.com>
2013-09-27 20:13:41 +0200

    Soft References

--- tasks-source/2-refactoring-makefile/Main.java
+++ tasks-source/2-refactoring-makefile/Main.java
@@ -1,60 +1,57 @@
 package pal;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.ref.SoftReference;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.TreeMap;
 
 public class Main
 {
-	static  String newline = System.getProperty("line.separator");
-
-    //static  int                   BIG_ENOUGHT_LINES       =  100000;
-    static  int                   BIG_ENOUGHT_NEIGHBOURS  =  100;
+	static  String                newline                 =  System.getProperty("line.separator");
+    static  int                   BIG_ENOUGHT_NEIGHBOURS  =  3000;
     static  BufferedReader        bi                      =  new BufferedReader(new InputStreamReader(System.in));
     static  StringBuilder         buffer                  =  new StringBuilder();
-    static  HashMap<String,Node>  cache                   =  new HashMap<String,Node>();
+    static  Cache                 cache                   =  new Cache();
     static  ArrayList<String>     line_map                =  new ArrayList<String>();
-    static  ArrayList<String>     relation                =  new ArrayList<String>();
+    static  ArrayList<Integer>    relation                =  new ArrayList<Integer>();
     static  Queue<Node>           q                       =  new LinkedList<Node>();
+    static  int                   FIRST                   =  0;
+    static  String                name                    =  "";
     
-    static  String                FIRST;
-    static  int                   LINES_RED;
-    static  String                name;
-    static  int                   current;
-    
-    static  HashSet<String> marker = new HashSet<String>();
+    static  HashSet<Node> marker = new HashSet<Node>();
 
     public static void main(String[] args)
     {
-    	line_map . clear();
-    	marker   . clear();
-    	relation . clear();
-        buffer   . setLength(0);
-        cache    . clear();
-        q        . clear();
+    	line_map  .  clear();
+    	marker    .  clear();
+    	relation  .  clear();
+        buffer    .  setLength(0);
+        //cache     .  clear();
+        q         .  clear();
+        
+        name      =  "";
+        FIRST     =  0;
 
-        name      = "";
-        FIRST     = "";
-        LINES_RED = 0;
-        current   = 0;
-    	
         try
         {
             slurp();
 
             Node root = cache.get(FIRST);
 
-            if(root==null) throw new FatalError();
-            
             non_declared();
+            
             traverse(root);
+    		q.clear();
             
             if(cycle(root)) throw new FatalError();
-
+            marker.clear();
+            
             out();
         }
         catch(FatalError e){ System.out.println("ERROR"); }
@@ -62,132 +59,109 @@ public class Main
 
     static boolean cycle(Node node)
     {
-    	if(node==null) return false;
+    	if(marker.contains(node)) return true;
     	
-    	if(marker.contains(node.data)) return true;
-    	
-    	marker.add(node.data);
+    	marker.add(node);
     	
     	for(int i=0; i<node.index; i++)
     	{
-    		if(!cache.containsKey(node.link[i].data)) return true;
+    		if(!cache.containsKey(node.link[i].id)) return true;
     		
     		if(!node.link[i].marked)
     		{
     			node.link[i].marked = true;
-    			if(cycle(node.link[i]))
-    				return true;
+    			if(cycle(node.link[i])) return true;
     		}
     	}
 	
-    	marker.remove(node.data);
+    	//marker.remove(node.data.hashCode());
+    	marker.remove(node);
     	return false;
     }
     
-    //OK
     private static void non_declared()
     {
-    	for(Node n : cache.values())
-    		if(n.visited && !n.isDeclared && n.isDependency) throw new FatalError();
+    	Collection<SoftReference<Node>> c = cache.values();
+    	
+    	for(SoftReference<Node> node : c)
+    	{
+    		Node n = node.get();
+    		if(!n.isDeclared && n.isDependency)  throw new FatalError();
+    	}
+    	
+    	c = null;
     }
 
-    //OK
     private static Node node(String key)
-    {
-        key     = key.trim();
-        Node n  = cache.get(key);
+    { return cache.get(key.trim().hashCode()); }
 
-        if(n==null)
-        {
-            n = new Node(key);
-            cache.put(key, n);
-        }
-        return n;
-    }
-
-    //FIXME breaks at ~50ms sooooo it should be somewhere there
     private static void readLine(String line)
 	{
-    	
-    	LINES_RED++;
         line_map.add(line);
-
+        
         if(line.startsWith("\t"))
         {
-        	relation.add(name);
+        	relation.add(name.hashCode());
         }
         else
         {
+        	
         	int index				= line.indexOf(":");
-        	
         	if(index<=0) return;
-        	
         	name					= line.substring(0, index);
-                
-        	if(FIRST.equals("")) FIRST=name;
-            
-        	//FIXME there could be a exception
+        	
+        	if(FIRST==0) FIRST = name.hashCode();
+        	
         	String[] dependencies	= line.substring(index).split(" ");
         	Node n				    = node(name);
-				
-        	n.isDeclared = true;
+        	n.isDeclared            = true;
 
-        	//OK ?
-        	//if(dependencies.length>1)
         	for(int i=1; i<dependencies.length; i++)
         	{
-        		//OK
-        		if(dependencies[i]==null || dependencies[i].isEmpty()) continue;
+        		if(dependencies[i].isEmpty()) continue;
                     
         		Node m = node(dependencies[i]);
         		n.addEdge(m);
         		m.isDependency = true;
         	}
         	
-        	relation.add(name);
+        	relation.add(name.hashCode());
         }
     }
 
-    //OK
     public static void slurp()
     {
-    	String line="";
-    	try
-    	{
-    		while((line=bi.readLine()) != null)
-    			readLine(line);
-    	}
-    	catch(Exception e){/*ignore*/}
+    	try						{ while(true) readLine(bi.readLine()); }
+    	catch(Exception e)		{/*ignore*/}
+    	
+    	try						{ bi.close(); }
+    	catch (IOException e)	{/*ignore*/}
+    	finally					{ bi = null; }
+    	
+    	System.runFinalization();
+    	System.gc();
     }
     
-    
-    //OK
-    static void out()
+    static void out() throws FatalError
     {
         buffer.setLength(0);
 
-        Node n = null;
-
-        for(int i=0; i<LINES_RED; i++)
+        Node n    = null;
+        int size  = relation.size();
+        
+        for(int i=0; i<size; i++)
         {
-        	//FIXME there could be a exception
 			n = cache.get(relation.get(i));
-			
 			if(n==null) continue;
 
 			if(n.visited) 	buffer . append(line_map.get(i)     + newline);
 			else			buffer . append("#"+line_map.get(i) + newline);
         }
-        
         System.out.print(buffer);
     }
 
-    
 	public static void traverse(Node node) throws FatalError
 	{
-		//FIXME need?
-		if(node==null) return;
-		
 		q.clear();
 		q.add(node);
 		node.visited	= true;
@@ -213,29 +187,46 @@ public class Main
 
     static class Node
     {
-		String   data          =  "";
 		boolean  visited       =  false;
 		boolean  marked	       =  false;
 		boolean  isDependency  =  false;
 		boolean  isDeclared    =  false;
 		Node[]   link          =  new Node[BIG_ENOUGHT_NEIGHBOURS];
 		int      index         =  0;
+		int      id            =  0;
 		
-		public Node(String data)
-		{ this.data = data; }
+		public Node(int id)
+		{ this.id = id; }
 	
 		public void addEdge(Node b)
 		{ link[index++] = b; }
-	
-		public java.lang.String toString()
-		{ return data . toString(); }
-
-		public boolean equals(Object another)
-		{ return (this.hashCode() == another.hashCode()) || (((Node)another) . data.equals(this . data)); }
+		
+		public boolean equals(Object other)
+		{ return ((Node)other).id==this.id; }
 		
 		public int hashCode()
-		{ return this.data.hashCode(); }
+		{ return this.id; }
 
 	}
 
+    static class Cache
+    { 
+        private TreeMap<Integer, SoftReference<Node>> cache = new TreeMap<Integer, SoftReference<Node>>();
+
+        public Node get(int key)
+        {
+        	Node node = null;
+        	SoftReference<Node> ref = cache.get(key);
+            if(ref != null)		node = ref.get();
+            if(node == null)	cache.put(key, new SoftReference<Node>(node = new Node(key)));
+            return node;   
+        }
+
+		public Collection<SoftReference<Node>> values()
+		{ return cache.values(); }
+
+		public boolean containsKey(int id)
+		{ return cache.containsKey(id); }
+
+    }
 }
